e7bdf83e52aecdb627c2083a1377d288f79b9389
Fliiiiix <francois-xavier.sabatier@efrei.net>
2025-02-10 09:41:41 +0100

    Multiples ajouts
    Ajouts de la classe jeu qui permet de lancer un écran avec différentes fonctions : FPS, position d'un objet, nom du jeu sur l'écran d'affichage, déplacement de haut en bas et ajout d'un fond.
    Utilisation d'assets qui ne sont pas les nôtres et qui ne seront pas utilisés pour notre version finale du jeu mais qui nous permettent de commencer le code.

--- game.py
+++ game.py
@@ -1,5 +1,45 @@
 import pygame
 
-pygame.init()
+import sys 
 
-screen = pygame.display.set_mode((800, 600))
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption("Anima") #Nom de la fenêtre
+        self.screen = pygame.display.set_mode((800, 600))
+
+        self.clock = pygame.time.Clock()
+
+        self.img = pygame.image.load("Data\data\images\clouds\cloud_1.png") #Chargement de l'image
+        self.img.set_colorkey((0,0,0)) #Couleur de transparence
+
+        self.img_pos = [160, 260]
+        self.movement = [False, False]
+
+    def run(self):
+        running = True
+
+        while running: #Boucle qui sert à actualiser le jeu
+            self.screen.fill((14, 219, 248)) #Couleur de fond et clear de l'écran
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5 #Déplacement de l'image
+            self.screen.blit(self.img,self.img_pos) #Affiche l'image à la position donnée
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:   #Quitte le jeu si la touche en question est appuyée
+                    running = False 
+                    sys.exit()  #Quitte le programme
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
+            pygame.display.update()
+            self.clock.tick(60) #Actualise le jeu 60 fois par seconde
+
+Game().run()
