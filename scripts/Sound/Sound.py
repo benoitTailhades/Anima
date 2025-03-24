3c0efa679dd0d1f365b33f5c0d58659cc1b2a5b3
Fliiiiix <francois-xavier.sabatier@efrei.net>
2025-03-24 08:31:55 +0100

    Sound py

---
+++ scripts/Sound/Sound.py
@@ -0,0 +1,6 @@
+import pygame
+
+pygame.mixer.init()
+
+Explosion_Large_Blast_1__6s = pygame.mixer.Sound("assets\sounds\effects\Explosions\Explosion_Large_Blast_1__6s.wav")
+Explosion_Large_Blast_1__6s.play()
