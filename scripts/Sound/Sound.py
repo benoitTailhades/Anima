20481d9b311e215390dff0c713800f1a5f9a0bd8
Fliiiiix <francois-xavier.sabatier@efrei.net>
2025-03-24 08:49:01 +0100

    Sound py

--- scripts/Sound/Sound.py
+++ scripts/Sound/Sound.py
@@ -2,5 +2,5 @@ import pygame
 
 pygame.mixer.init()
 
-Mer = pygame.mixer.Sound("../assets/sounds/Vagues/AMBSea_Mer_vagues_moyennes_et_mouettes_(ID 0267)_LS.wav")
+Mer = pygame.mixer.Sound("../../assets/sounds/Vagues/AMBSea_Mer_vagues_moyennes_et_mouettes_(ID 0267)_LS.wav")
 Mer.play()
